50e8cb04d1e97b78ac891b2e3ff0e9c9688ce668
JairoSanchez <jairosanchezb5@gmail.com>
2022-04-11 09:23:25 +0200

    subida de PokemonApi

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import classnames from 'classnames';
+
+const Branding = styled.a`
+  -moz-user-select: none;
+  -website-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
+
+export default class NavBar extends Component {
+  state = {
+    hoverNavBar: false
+  };
+
+  hoverNavBar() {
+    window.scrollY <= 0
+      ? this.setState({ hoverNavBar: false })
+      : this.setState({ hoverNavBar: true });
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.hoverNavBar.bind(this), true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.hoverNavBar.bind(this), true);
+  }
+
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-md navbar-dark bg-dark fixed-top"
+        style={
+          this.state.hoverNavBar
+            ? {
+                boxShadow:
+                  '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+                transition: 'all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)',
+                backgroundColor: '#3352FF !important'
+              }
+            : { backgroundColor: 'transparent !important' }
+        }
+      >
+        <Branding
+          href="#"
+          className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
+        >
+          Pokedex Atrinium
+        </Branding>
+      </nav>
+    );
+  }
+}
